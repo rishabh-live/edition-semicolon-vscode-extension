cf452d7db5a5a3bc4428a07e632d1a2044303ec4
rishabh-live <rishabh12536@gmail.com>
2020-12-11 15:59:02 +0530

    Version One

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Edition-Semicolon VS Code Extension
+
+### What is Edition Semicolon?
+
+> Edition Semicolon is a tool to write articles anonymously over IPFS Network.
+> Edition Semicolon Doesn't uses Database to record anything, niether you need an account to get started
+
+### What is Edition Semicolon Extension?
+
+> Edition Semicolon VS Code Extension helps you to publish your files over IPFS Network.
+> Even your HTML, CSS, JS, and anything.
+
+### Does it uploads images and videos too?
+
+> Ammmm ! Not at momment but maybe future....!!!
---
+++ extension.js
@@ -0,0 +1,63 @@
+console.log('Congratulations, your extension "edition-semicolon" is now active!');
+const { default: fetch } = require('node-fetch');
+const vscode = require('vscode');
+// const fetch = require('node-fetch');
+
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+// @ts-ignore
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+
+    console.log('Congratulations, your extension "edition-semicolon" is now active!');
+
+
+    const disposable = vscode.commands.registerCommand('edition-semicolon.helloWorld', async() => {
+        //IPFS Declaration 
+
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showInformationMessage(`No Content Found.`);
+            return;
+        }
+        const content = editor.document.getText(editor.selection);
+        await fetch("https://edition-semicolon.rishabh.live/api/publish", {
+
+            // Adding method type 
+            method: "POST",
+
+            // Adding body or contents to send 
+            body: JSON.stringify({
+                "cnt": content
+            })
+        })
+
+        // Converting to JSON 
+        .then(response => response.json())
+
+        // Displaying results to console 
+        .then(json => {
+
+            vscode.window.showInformationMessage(`Content Published.  Your Content ID : ${json.id}`);
+            console.log(`Content Published.  Your Content ID : ${json.id}`);
+        });
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
